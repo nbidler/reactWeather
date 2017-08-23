691aadaaf9e06ab163fddf89cf4b2fdba7983e05
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2017-08-22 18:32:44 -0700

    About page, added links to resources

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -24,8 +24,24 @@ var React = require('react');
 var about = (props) => {
     return(
         <div>
-            <h3>About</h3>
-            <p>Welcome the to the about page - the only thing it's about is itself</p>
+            <h1 className="text-center">About</h1>
+            <p>This is just a small project to query a <a href="http://openweathermap.org/api">free weather API</a> to give temperature information.</p>
+            <p>The <i>reason</i> I did this was to get my feet wet using <a href="https://facebook.github.io/react/">ReactJS</a>, because (at time of writing)
+                it is one of the big three javascript libraries; hopefully knowing all three will let me net stable employment and enough cash to
+                live on my own ( <a href="http://www.nbidler.com">hit me up</a> if you know of web developer jobs, whether or not they're somewhere
+                cheap to live).</p>
+            <p>Special thanks to:</p>
+            <ul>
+                <li>
+                    <a href="https://www.udemy.com/the-complete-react-web-app-developer-course">The Complete React Web App Developer Course</a>
+                </li>
+                <li>
+                    <a href="http://openweathermap.org/api">OpenWeatherMap.org for not charging to use their API</a>
+                </li>
+                <li>
+                    <a href="https://facebook.github.io/react">The developers of React</a>
+                </li>
+            </ul>
         </div>
     )
 };
